e68cc9414b050a5a0f5e51164863006c5f39473b
oemss <thispagenotfound@yandex.ru>
2018-01-14 15:49:12 +0300

    add DB

--- stats/admin.py
+++ stats/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Stat
 # Register your models here.
+
+admin.site.register(Stat)
--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Stat(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=200)
+    tag = models.CharField(max_length=1000)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+    
